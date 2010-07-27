6e5c1ccc5d6a355cecb394ae899c9a8b698eacf6
Paul Levold <paal.levold@gmail.com>
2010-07-27 21:26:45 +0200

    Licence headers and readme file

--- GCal.py
+++ GCal.py
@@ -1,4 +1,26 @@
-__author__ = 'paal.levold@gmail.com (Paul Levold)'
+''''
+		GCal.py
+		
+		Copyright 2010 Pål Levold		
+		
+    This file is part of ics2gCal.
+
+    ics2gCal is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ics2gCal is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+    
+    This file is based on calendarExample.py from Google Inc. in the GData
+    Python API <http://code.google.com/p/gdata-python-client/downloads/list>.
+''''
 
 
 try:
